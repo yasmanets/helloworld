904a5bc960f801be2658f4657e7d5176cef60a8a
yaser-eda-teamswipe <yaser.94ae@gmail.com>
2022-05-06 10:20:31 +0200

    User model

---
+++ hello-world/hello-world/Models/Users.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  hello-world
+//
+//  Created by Yaser El Dabete Arribas on 5/5/22.
+//
+
+import Foundation
+
+struct Users: Codable {
+    let id: Int?
+    let name: String?
+    let birthdate: String?
+}
